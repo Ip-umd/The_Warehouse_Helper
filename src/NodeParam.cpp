750f650e8fb0462ca2b70e96997b767419a930a0
Ip-umd <ip1996@terpmail.umd.edu>
2019-12-09 05:32:37 -0500

    [NodeParam.cpp] [Task 3.10] Implemented the NodeParam class.

--- src/NodeParam.cpp
+++ src/NodeParam.cpp
@@ -1,43 +1,3 @@
-/**
- * The 3-Clause BSD License
- *
- * Copyright (c) 2019, Ishan Patel, Nakul Patel, Sri Manika Makam
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- * contributors may be used to endorse or promote products derived from this
- * software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- */
-
-/**
- *  @file      NodeParam.cpp
- *  @author    Sri Manika Makam
- *  @copyright 3-Clause BSD License
- *  @brief     Implementation of NodeParam class.
- */
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -52,27 +12,35 @@ NodeParam::~NodeParam() {
 }
 
 std::vector<double> NodeParam::getCurrent() {
+  return currentNode;
 }
 
 void NodeParam::setCurrent(std::vector<double> current) {
+  currentNode = current;
 }
 
 std::vector<double> NodeParam::getParent() {
+  return parentNode;
 }
 
 void NodeParam::setParent(std::vector<double> parent) {
+  parentNode = parent;
 }
 
 double NodeParam::getGCost() {
+  return gCost;
 }
 
 void NodeParam::setGCost(double costToGo) {
+  gCost = costToGo;
 }
 
 double NodeParam::getHCost() {
+  return hCost;
 }
 
 void NodeParam::setHCost(double costToCome) {
+  hCost = costToCome;
 }
 
 double NodeParam::getTotalCost() {
@@ -80,9 +48,11 @@ double NodeParam::getTotalCost() {
 }
 
 void NodeParam::setTotalCost(double total) {
+  totalCost = total;
 }
 
 double NodeParam::getTheta() {
+  return theta;
 }
 
 void NodeParam::setTheta(double th) {
@@ -90,20 +60,25 @@ void NodeParam::setTheta(double th) {
 }
 
 double NodeParam::getDTheta() {
+  return dtheta;
 }
 
 void NodeParam::setDTheta(double dth) {
+  dtheta = dth;
 }
 
 double NodeParam::getDx() {
+  return dx;
 }
 
 void NodeParam::setDx(double dX) {
+  dx = dX;
 }
 
 double NodeParam::getDy() {
+  return dy;
 }
 
 void NodeParam::setDy(double dY) {
+  dy = dY;
 }
-
